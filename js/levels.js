db8676ddb9a387fe1f584959f54bb7b78062a675
Michal Pokorný <pok@rny.cz>
2016-01-30 17:20:59 +0100

    fix level format

--- js/levels.js
+++ js/levels.js
@@ -1,26 +1,28 @@
 var levels = {
-    "test": {
-            	threads: [
-            		[
-            			new Instruction("Hello World!"),
-            		],
-            		[
-            			new Instruction("foo"),
-            			new Instruction("bar"),
-            			new Instruction("zoo")
-            		],
-            		[
-            			new Instruction("bar"),
-            			new Instruction("foo"),
-            			new Instruction("bar"),
-            		]
-            	]
-            },
-    "tutorial": {
-            threads : [
-                [
-                    new Instruction("This is the single-thread tutorial.")
-                ]
-            ]
-        }
-}
+	"test": new Level(
+		"In this level, you slay monsters.",
+		[
+			new Thread([
+				new Instruction("Hello World!"),
+			]),
+			new Thread([
+				new Instruction("foo"),
+				new Instruction("bar"),
+				new Instruction("zoo"),
+				new WinningInstruction("[REACH THIS TO WIN]")
+			]),
+			new Thread([
+				new Instruction("bar"),
+				new WinningInstruction("[OR REACH THIS TO WIN]"),
+				new Instruction("foo"),
+				new Instruction("bar"),
+			])
+		]),
+	"tutorial": new Level(
+		"This is the tutorial level.",
+		[
+			new Thread([
+				new Instruction("This is the single-thread tutorial.")
+			])
+		])
+};
