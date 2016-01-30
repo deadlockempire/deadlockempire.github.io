298d000635bc691daaa7ee2d300838c8cf078c6b
Petr Hudecek <petrhudecek2010@gmail.com>
2016-01-30 17:22:17 +0000

    main menu updates

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,9 @@
+function startLevelCreator(level) {
+    return function() {
+        startLevel(level);
+    }
+}
+
 var returnToMainMenu = function() {
     $("#mainarea").html("");
     for (var levelId in levels) {
@@ -8,9 +14,12 @@ var returnToMainMenu = function() {
 		source.append("<span class='mainMenuLevelDescription'>" + level.intro + "</span>");
 		source.append("<br>");
 		var button = $('<button>Play</button>');
-		button.click(function () {
-            startLevel(level);
-		});
+		button.click(startLevelCreator(level));
+		if (localStorage.getItem['level_' + levelId]) {
+		    button.html("Replay");
+		    source.append("This level has been completed.<br>");
+		    source.addClass('mainMenuSolvedLevel');
+		}
 		source.append(button);
 		$("#mainarea").append(source);
     }
