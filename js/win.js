a1004a3b47315e6c65333b0f96857ccdbb5420d9
Michal Pokorný <pok@rny.cz>
2016-02-04 12:46:48 +0100

    Add 'lose'

--- js/win.js
+++ js/win.js
@@ -1,4 +1,5 @@
 var winScreen;
+var loseScreen;
 
 var win = function(reason) {
 	if (levelWasCleared) {
@@ -8,15 +9,10 @@ var win = function(reason) {
 	winScreen.fadeIn(400);
 	levelWasCleared = true;
 
-	//$('#win-screen .icon').slideDown();
-	//
-
 	var messages = ["You win!", "Great job!", "Congratulations!"];
 	var randomMessage = messages[Math.floor(3 * Math.random())];
 
 	$('#win-congratulation').text(randomMessage);
-
-	// TODO: taky popsat, jak jsem to znicil
 
 	var text = gameState.getLevel().victoryText;
 	if (reason) {
@@ -37,6 +33,18 @@ var areThereMoreLevels = function() {
 	return findNextLevelInCampaign(gameState.getLevelId()) != null;
 };
 
+var lose = function(reason) {
+	loseScreen.fadeIn(400);
+	var text;
+	if (gameState.getLevel().failureText) {
+		text = gameState.getLevel().failureText;
+		if (reason) {
+			text += "<br><br><p>Failure Condition: <i>" + reason + "</i></p>";
+		}
+	}
+	$("#lose-message").html(text);
+};
+
 $(function() {
 	winScreen = $('#win-screen');
 	winScreen.css({display: 'none'});
@@ -53,4 +61,16 @@ $(function() {
 		returnToMainMenu();
 		winScreen.fadeOut(300);
 	});
+
+	$('#lose-restart').click(function() {
+		resetLevel();
+		loseScreen.fadeOut(300);
+	});
+
+	$('#lose-go-to-menu').click(function() {
+		returnToMainMenu();
+		loseScreen.fadeOut(300);
+	});
+
+	loseScreen = $('#lose-screen');
 });
